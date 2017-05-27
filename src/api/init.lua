d482bfa6d2b62e4ac25295d8a3d1b452a3531936
crito <crito@fnordpipe.org>
2017-05-27 20:34:29 +0200

    api: add config file inclusion

--- src/api/init.lua
+++ src/api/init.lua
@@ -1,19 +1,17 @@
 -- lapis
 local lapis = require('lapis')
 local json_params = require('lapis.application').json_params
-
--- fs
 local lfs = require('lfs')
+local config = require('config')
 
 local api = {
   app = lapis.Application()
 }
 
 function api.init(self)
-  versionDir = './endpoints'
-  package.path = package.path .. ';' .. versionDir .. '/?.lua'
-  for version in lfs.dir(versionDir) do
-    moduleDir = versionDir .. '/' .. version
+  package.path = package.path .. ';' .. config.endpoints .. '/?.lua'
+  for version in lfs.dir(config.endpoints) do
+    moduleDir = config.endpoints .. '/' .. version
     if version ~= '.' and version ~= '..' and lfs.attributes(moduleDir, 'mode') == 'directory' then
       for endpoint in lfs.dir(moduleDir) do
         moduleFile = moduleDir .. '/' .. endpoint
