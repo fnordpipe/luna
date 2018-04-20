8cf68755baeb31dc60c3af9b8ec759437c75a96c
crito <crito@fnordpipe.org>
2018-04-20 20:58:13 +0200

    update example endpoint interface

--- src/endpoints/example/test.lua
+++ src/endpoints/example/test.lua
@@ -1,15 +1,15 @@
 local test = {}
 
 function test.sayHi(self)
-  return { status = 200, json = { msg = 'Hi' } }
+  return ngx.HTTP_OK, { msg = 'Hi' }
 end
 
 function test.sayBye(self)
-  return { status = 200, json = { msg = 'Bye' } }
+  return ngx.HTTP_OK, { msg = 'Bye' }
 end
 
 test.routes = {
-  { context = '', method = 'GET' call = test.sayHi },
+  { context = '', method = 'GET', call = test.sayHi },
   { context = '/hi', method = 'POST', call = test.sayHi },
   { context = '/bye', method = 'DELETE', call = test.sayBye }
 }
